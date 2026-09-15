5283b5b0c46ee875c75441084a3d7110d109ad2c
ZUMARRAGA PUENTE JOSE DANIEL <JDZUMARRAGA@PUCE.EDU.EC>
2026-09-15 15:25:05 -0500

    feat: add players CRUD (controller, routes, views) with LaLiga theme

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    public function index()
+    {
+        $players = Player::with('team')->get();
+        return view('players.index', compact('players'));
+    }
+
+    public function create()
+    {
+        $teams = Team::all();
+        return view('players.create', compact('teams'));
+    }
+
+    public function store(Request $request)
+    {
+        Player::create($request->validate([
+            'name' => 'required',
+            'position' => 'nullable',
+            'nationality' => 'nullable',
+            'photo' => 'nullable',
+            'team_id' => 'required|exists:teams,id',
+        ]));
+        return redirect()->route('players.index');
+    }
+
+    public function edit(Player $player)
+    {
+        $teams = Team::all();
+        return view('players.edit', compact('player', 'teams'));
+    }
+
+    public function update(Request $request, Player $player)
+    {
+        $player->update($request->validate([
+            'name' => 'required',
+            'position' => 'nullable',
+            'nationality' => 'nullable',
+            'photo' => 'nullable',
+            'team_id' => 'required|exists:teams,id',
+        ]));
+        return redirect()->route('players.index');
+    }
+
+    public function destroy(Player $player)
+    {
+        $player->delete();
+        return redirect()->route('players.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/players');
+Route::resource('players', PlayerController::class)->except(['show']);
